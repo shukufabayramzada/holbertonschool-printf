c8a33d9e70307d3c39d89abd19f5e4faa9be3530
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-27 10:00:32 +0000

    header file was created

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct specifiers - Struct op
+ * @spec: format specifiers that we check
+ * @func: The function associated
+ **/
+typedef struct specifiers
+{
+	char *spec;
+	int (*func)(va list arg);
+} spec_type;
+#endif
