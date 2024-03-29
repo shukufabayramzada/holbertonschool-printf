21c9f4c6225612ef6adc179d9fe8b0b95de02856
sayka88 <sayka.alekperova@gmail.com>
2024-03-29 11:58:24 +0000

    some change in printf.c for betty style

--- printf.c
+++ printf.c
@@ -45,7 +45,6 @@ int _printf(const char *format, ...)
 
 	if (!format)
 	{
-		va_end(args);
 		return (-1);
 	}
 	for (i = 0; format[i] != '\0'; i++)
@@ -53,24 +52,33 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-			va_end(args);
+			{
 			return (-1);
+			}
 			else if (format[i + 1] == '%')
-			_putchar('%');
+			{
+				_putchar('%');
 			count++;
 			i++;
+			}
 			else if (cmp_func(format[i + 1]) != NULL)
+			{
 			count += (cmp_func(format[i + 1]))(args);
 			i++;
+			}
 			else
+			{
 			_putchar('%');
 			_putchar(format[i + 1]);
 			count += 2;
 			i++;
+			}
 		}
 		else
+		{
 		_putchar(format[i]);
 		count++;
+		}
 	}
 	va_end(args);
 	return (count);
