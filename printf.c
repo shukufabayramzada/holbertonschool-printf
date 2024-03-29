340978b6c1ae52aaba20a00b9f1c7d60ff3d0c16
sayka88 <sayka.alekperova@gmail.com>
2024-03-29 11:53:03 +0000

    some change in printf.c for betty style

--- printf.c
+++ printf.c
@@ -39,49 +39,38 @@ int _printf(const char *format, ...)
 {
 	int i = 0;
 	int count = 0;
-	va_list(args);
 
+	va_list(args);
 	va_start(args, format);
 
 	if (!format)
 	{
 		va_end(args);
 		return (-1);
-	}	
+	}
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
+			va_end(args);
+			return (-1);
 			else if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				count++;
-				i++;
-			}
+			_putchar('%');
+			count++;
+			i++;
 			else if (cmp_func(format[i + 1]) != NULL)
-			{
-				count += (cmp_func(format[i + 1]))(args);
-				i++;
-			}
+			count += (cmp_func(format[i + 1]))(args);
+			i++;
 			else
-			{
-				_putchar('%');
-				_putchar(format[i + 1]);
-				count += 2;
-				i++;
-			}
+			_putchar('%');
+			_putchar(format[i + 1]);
+			count += 2;
+			i++;
 		}
 		else
-		{
-	
-			_putchar(format[i]);
-			count++;
-		}
+		_putchar(format[i]);
+		count++;
 	}
 	va_end(args);
 	return (count);
