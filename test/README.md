6a2c495e2543f06dee54c9e5f6843638b445da22
sayka88 <sayka.alekperova@gmail.com>
2024-03-29 17:42:33 +0000

    creat test folder and check test a.out file

---
+++ test/README.md
@@ -0,0 +1,1 @@
+REaDME file
---
+++ test/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct specifiers - Struct for specifiers
+ * @spec: format specifiers that we check
+ * @func: The function associated
+ **/
+typedef struct specifiers
+{
+	char spec;
+	int (*func)(va_list);
+} spec_type;
+
+int _printf(const char *format, ...);
+int (*cmp_func(const char s))(va_list);
+#endif
